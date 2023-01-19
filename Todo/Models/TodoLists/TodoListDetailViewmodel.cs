a080ec1635dd089b2e068d2ffb84995961e0da65
Daniil Hasiul <dgasyul@gmail.com>
2023-01-19 17:03:58 +0530

    show done items based on option

--- Todo/Models/TodoLists/TodoListDetailViewmodel.cs
+++ Todo/Models/TodoLists/TodoListDetailViewmodel.cs
@@ -8,12 +8,14 @@ namespace Todo.Models.TodoLists
         public int TodoListId { get; }
         public string Title { get; }
         public ICollection<TodoItemSummaryViewmodel> Items { get; }
+        public bool ShouldShowDoneItems { get; }
 
-        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items)
+        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items, bool shouldShowDoneItems)
         {
             Items = items;
             TodoListId = todoListId;
             Title = title;
+            ShouldShowDoneItems = shouldShowDoneItems;
         }
     }
 }
